7912659cf53950f657cec5e594d15e8183567f67
Makoa <32736507+santarini@users.noreply.github.com>
2018-07-21 14:08:42 -1000

    Update scatter.py

--- scatter.py
+++ scatter.py
@@ -1,1 +1,12 @@
+import matplotlib.pyplot as plt
 
+x=[1,2,3,4,5,6,7]
+y=[3,6,7,4,5,6,9]
+
+plt.scatter(x,y, label="stuff", color='red')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('TIYLE')
+plt.legend()
+plt.show()
